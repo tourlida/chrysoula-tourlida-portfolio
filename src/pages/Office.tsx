46e0439f5809ddd33a86dae3791bd28d481f5c3c
Vagia Tourlida <vagiatourlida@hotmail.com>
2023-08-11 12:15:12 +0300

    Fix loading issue in images at office section

--- src/pages/Office.tsx
+++ src/pages/Office.tsx
@@ -23,37 +23,38 @@ const itemData = [
   {
     imgPath: "/static/office_3.jpg",
   },
-  
 ];
 export default function Office() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  
-  const titleEl = useMemo(()=>{
-    return <Stack justifyContent="center" alignItems="center" mb="32px">
-    <Typography
-      component="div"
-      sx={{
-        width: "100%",
-        textAlign: "center",
-        fontWeight: 300,
-        fontSize : isMobile ? '32px' : '36px'
-      }}
-      variant="h4"
-    >
-     Το Ιατρείο μας{" "}
-    </Typography>
-    <div
-      style={{
-        marginTop:'16px',
-        height: "4px",
-        width: "262px",
-        background:
-          "linear-gradient(90deg, rgba(111,168,184,1) 0%, rgba(61,114,128,1) 100%)",
-      }}
-    ></div>
-  </Stack>
-  },[isMobile]);
+
+  const titleEl = useMemo(() => {
+    return (
+      <Stack justifyContent="center" alignItems="center" mb="32px">
+        <Typography
+          component="div"
+          sx={{
+            width: "100%",
+            textAlign: "center",
+            fontWeight: 300,
+            fontSize: isMobile ? "32px" : "36px",
+          }}
+          variant="h4"
+        >
+          Το Ιατρείο μας{" "}
+        </Typography>
+        <div
+          style={{
+            marginTop: "16px",
+            height: "4px",
+            width: "262px",
+            background:
+              "linear-gradient(90deg, rgba(111,168,184,1) 0%, rgba(61,114,128,1) 100%)",
+          }}
+        ></div>
+      </Stack>
+    );
+  }, [isMobile]);
 
   return (
     <section
@@ -61,19 +62,22 @@ export default function Office() {
       id="office"
       style={{
         height: "fit-content",
-        padding:'64px 0',
-        margin:'auto'
+        padding: "64px 0",
+        margin: "auto",
       }}
     >
       {titleEl}
-      <Box sx={{ width:'75%', margin: "auto", height: "fit-content" }}>
+      <Box sx={{ width: "75%", margin: "auto", height: "fit-content" }}>
         <ImageList variant="masonry" cols={3} gap={8}>
           {itemData.map((item) => (
             <ImageListItem key={item.imgPath}>
-            <img
-                src={`${item.imgPath}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.imgPath}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={'Office'}
+              <Image
+                src={item.imgPath}
+                height={248}
+                width={248}
+                layout="responsive"
+                quality={100}
+                alt={"Office"}
                 loading="lazy"
               />
             </ImageListItem>
