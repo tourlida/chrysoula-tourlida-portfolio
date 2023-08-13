fc43a179eee79d13227dd7a16e8b6ebce81f3230
Vagia Tourlida <vagiatourlida@hotmail.com>
2023-08-13 21:40:30 +0300

    Seo optimization v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,11 @@ const LandingPage = () => {
     <>
       <Head>
         <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8"/>
+        <meta name="google-site-verification" content="I21XqSDecJvksgT0f1qLa73oQbeD-fPb8aJ_nVXAMS8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content={metadata.description}/>
+        <meta name="keywords" content={metadata.keywords} /> 
       </Head>
       <Stack>
         <Home />
